fb0387c51773edf18bf9cd8dfda47dc83cf791e7
angela.oliveira <angela.oliveira@assert.ifpb.edu.br>
2021-06-16 19:22:51 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const cepField = document.querySelector('#cep')
+const nome = document.querySelector('#nome')
+const main = document.getElementById('main')
+
+cepField.addEventListener('keyup', function(event) {
+  if (event.key == 'Enter') {
+    let value = cepField.value
+    let nomeValue = nome.value
+    // main.insertAdjacentHTML('beforeend', `
+    // <div class="card"><p>${nomeValue}</p></div>`)
+    pesqusiarCep(value, nomeValue)
+  }
+})
+
+
+function pesqusiarCep (cep, nome) {
+  let url = `https://viacep.com.br/ws/${cep}/json/`
+  fetch(url)
+    .then(res => res.json())
+    .then(cepInfo => {
+      if(cepInfo.erro) {
+        return false
+      } else {
+        const card =  `
+        <div class="card">
+        <h4>${nome} mora em</h4>
+          <p>${cepInfo.logradouro}</p>
+          <p>${cepInfo.bairro}</p>
+          <p>${cepInfo.localidade}</p>
+          <p>${cepInfo.uf} - Brasil</p>
+        </div>`
+        main.insertAdjacentHTML('beforeend', card)
+      }
+    })
+}
